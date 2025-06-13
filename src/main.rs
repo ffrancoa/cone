d23628b8ef834a2c2f2fb6ebb35aabdecd083ff9
ffrancoa <ffrancoa97@gmail.com>
2025-06-13 14:44:12 -0500

    Include rustyline to the team.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, this is the start of CX-01.");
+use rustyline::DefaultEditor;
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let mut rl = DefaultEditor::new()?;
+
+    loop {
+        let input = rl.readline("CX ❯ ");
+
+        match input {
+            Ok(_) => println!("Everything's good."),
+            Err(_) => {
+                println!("Oops... Something went wrong.");
+                break
+            }
+        }
+    }
+
+    Ok(())
 }
