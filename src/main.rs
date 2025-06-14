d2c90e21ca42afa2cb20c8eb270aa5a16981728f
ffrancoa <ffrancoa97@gmail.com>
2025-06-14 13:22:01 -0500

    Implement history to the REPL.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod cx;
 use crate::cx::io;
 use crate::cx::repl;
 
+const HISTORY_FILENAME: &str = ".cone_history";
+
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let commands: Vec<String> = vec![
         "CLEAN".into(),
@@ -16,10 +18,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         "PREVIEW".into(),
         "PROCESS".into(),
     ];
+
     let helper = repl::ReadLineHelper::new(commands.clone());
 
     let mut rl = Editor::new()?;
     rl.set_helper(Some(helper));
+
+    if rl.load_history(HISTORY_FILENAME).is_err() {
+        cx::io::print_error("No previous history. Creating a new one...");
+        std::fs::File::create(HISTORY_FILENAME)?;
+    }
 
     loop {
         let raw_input = rl.readline("CX ❯ ");
@@ -29,9 +37,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                 let args: Vec<&str> = raw_input
                     .split_whitespace()
                     .collect();
+                
                 if args.is_empty() {
                     continue
                 }
+
+                rl.add_history_entry(raw_input.trim())?;
+                println!("{}", raw_input.trim());
 
                 let cmd = args[0].to_ascii_uppercase();
 
@@ -48,5 +60,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
         }
     }
 
+    rl.save_history(HISTORY_FILENAME)?;
     Ok(())
 }
