bb381ab5e20c7b5de98a983e51ff97620e14b653
ffrancoa <ffrancoa97@gmail.com>
2025-06-14 13:35:48 -0500

    Improve the 'io' local crate.

--- src/cx/io.rs
+++ src/cx/io.rs
@@ -2,14 +2,20 @@ use std::fmt::Display;
 
 use crossterm::style::Stylize;
 
+/// Prints a succesfull-operation message to stdout.
+/// Styled with green text.
 pub fn print_info(msg: impl Display) {
-    println!("{} {}", "[info]:".bold().green(), msg)
+    println!("{} {}", "[info]:".bold().green(), msg.to_string().green())
 }
 
+/// Prints a warning message to stderr.
+/// Styled with yellow text.
 pub fn print_warn(msg: impl Display) {
-    println!("{} {}", "[warning]:".bold().yellow(), msg)
+    eprintln!("{} {}", "[warning]:".bold().yellow(), msg.to_string().yellow())
 }
 
+/// Prints an error message to stderr.
+/// Styled with red text.
 pub fn print_error(msg: impl Display) {
-    eprintln!("{} {}", "[error]:".bold().red(), msg)
+    eprintln!("{} {}", "[error]:".bold().red(), msg.to_string().red())
 }
