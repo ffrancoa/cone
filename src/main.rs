f11f071a9e8507772d746e40a3a172e56a8a850a
ffrancoa <ffrancoa97@gmail.com>
2025-06-13 21:35:47 -0500

    Implemented the command-matching structure.

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     let mut rl = DefaultEditor::new()?;
 
     loop {
-        let inputline = rl.readline("CX ❯ ");
+        let raw_input = rl.readline("CX ❯ ");
 
-        match inputline {
-            Ok(_) => {
-                io::print_info("Everything's good.")
+        match raw_input {
+            Ok(raw_input) => {
+                let args: Vec<&str> = raw_input
+                    .split_whitespace()
+                    .collect();
+                if args.is_empty() {
+                    continue
+                }
+                let cmd = args[0].to_ascii_uppercase();
+                match cmd.as_str() {
+                    "LOAD" => {
+                        io::print_info("You've entered the LOAD command.")
+                    }
+                    _ => {
+                        io::print_error(format!("Command {} does not exist.", cmd))
+                    }
+                }
             }
             Err(_) => {
                 io::print_error("Oops... Something went wrong.");
