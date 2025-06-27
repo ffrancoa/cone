c41bf28dfe5f61eccf29a0fc59bcc1ee62629b57
ffrancoa <ffrancoa97@gmail.com>
2025-06-26 23:16:26 -0500

    Add a 'load' command parser.

--- src/cx/cmd/load.rs
+++ src/cx/cmd/load.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::path::PathBuf;
 
 use clap::Args;
@@ -5,6 +6,7 @@ use polars::prelude::*;
 
 use crate::cx::io;
 
+const REQUIRED_FILE_EXT: &str = "csv";
 
 /// Arguments for the `load` subcommand.
 #[derive(Args, Debug)]
@@ -12,6 +14,7 @@ pub struct LoadCmd {
     /// Path of file to load.
     #[arg(
         short, long,
+        value_parser = file_parser,
         value_name = "FILE",
     )]
     file: Option<PathBuf>,
@@ -33,5 +36,19 @@ pub fn run(cmd: LoadCmd, _dataset: &mut DataFrame) {
         // TODO: Load multiple files if needed
     } else {
         io::print_error("no file or directory provided");
+    }
+}
+
+/// A `clap`-compatible parser that always expects a `.csv` file.
+fn file_parser(s: &str) -> Result<PathBuf, Box<dyn Error + Send + Sync + 'static>> {
+    let path = PathBuf::from(s);
+
+    // 1. check existence
+    if !path.exists() { return Err("file does not exist".into()) }
+
+    // 2. check extension (case‐insensitive)
+    match path.extension().and_then(|ext| ext.to_str()) {
+        Some(ext) if ext.eq_ignore_ascii_case(REQUIRED_FILE_EXT) => Ok(path),
+        _ => Err("provided file does not have a valid extension".into()),
     }
 }
